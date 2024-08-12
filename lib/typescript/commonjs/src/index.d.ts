d4d20f9aa2b0bfb0f164b189f6b4fa3541130ef8
Vahid <vahid.alamipour@bilue.com.au>
2024-08-12 20:12:01 +1000

    fix(): add built to repo

---
+++ lib/typescript/commonjs/src/index.d.ts
@@ -0,0 +1,3 @@
+import { type PropsWithChildren } from 'react';
+export declare const Usetiful: ({ children }: PropsWithChildren) => import("react/jsx-runtime").JSX.Element;
+//# sourceMappingURL=index.d.ts.map
